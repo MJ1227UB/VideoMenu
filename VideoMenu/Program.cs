c2d4ba3d3d775f37e63bc1fb45c7f7575e4867a3
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-08-17 10:53:56 +0200

    Added initial menu

--- VideoMenu/Program.cs
+++ VideoMenu/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace VideoMenu
 {
     class Program
     {
+        private static bool isUserDone = false;
+        private static List<Video> VideoList;
+        private static Dictionary<int, string> MenuItems;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (!isUserDone)
+            {
+                ShowMenu();
+                int selection = GetSelection();
+                ShowMenuItem(selection);
+            }
+        }
+
+
+        private static void ShowMenuItem(int selection)
+        {
+            switch (MenuItems)
+            {
+                    
+            }
+        }
+
+        private static int GetSelection()
+        {
+            int selection;
+            while (int.TryParse(Console.ReadLine(), out selection))
+            {
+                Console.WriteLine("Please enter a number:");
+            }
+            return selection;
+        }
+
+        private static void ShowMenu()
+        {
+            while (!isUserDone)
+            {
+                Console.WriteLine("Welcome to the video menu!\n" +
+                                  "Please select what you want to do:");
+            }
+            
         }
     }
 }
---
+++ VideoMenu/Video.cs
@@ -0,0 +1,14 @@
+﻿namespace VideoMenu
+{
+    public class Video
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public static int Id { get; private set; } = 0;
+
+        public Video()
+        {
+            ++Id;
+        }
+    }
+}
