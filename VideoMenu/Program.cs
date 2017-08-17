48c82c9d0d77b2a765acd01296e8a7a283fbff85
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-08-17 12:01:36 +0200

    Made a dynamic menu.

--- VideoMenu/Program.cs
+++ VideoMenu/Program.cs
@@ -7,10 +7,14 @@ namespace VideoMenu
     class Program
     {
         private static bool isUserDone = false;
-        private static List<Video> VideoList;
-        private static Dictionary<int, string> MenuItems;
+        private static List<Video> _videoList;
+        private static Dictionary<int, string> _menuItems;
+
         static void Main(string[] args)
         {
+            _menuItems = new Dictionary<int, string>();
+            _videoList = new List<Video>();
+            InitializeMenuItems();
             while (!isUserDone)
             {
                 ShowMenu();
@@ -19,33 +23,38 @@ namespace VideoMenu
             }
         }
 
+        private static void InitializeMenuItems()
+        {
+            _menuItems.Add(1, "Add new video");
+            _menuItems.Add(2, "Show all videos");
+            _menuItems.Add(3, "Edit a video");
+            _menuItems.Add(4, "Delete a video");
+        }
+
 
         private static void ShowMenuItem(int selection)
         {
-            switch (MenuItems)
-            {
-                    
-            }
+            Console.WriteLine($"\n{selection}. {_menuItems[selection]}\n");
         }
 
         private static int GetSelection()
         {
             int selection;
-            while (int.TryParse(Console.ReadLine(), out selection))
+            while (!int.TryParse(Console.ReadLine(), out selection) || !_menuItems.ContainsKey(selection))
             {
-                Console.WriteLine("Please enter a number:");
+                Console.WriteLine("Please enter a valid number:");
             }
             return selection;
         }
 
         private static void ShowMenu()
         {
-            while (!isUserDone)
+            Console.WriteLine("Welcome to the video menu!\n" +
+                              "Please select what you want to do:");
+            foreach (var menuItem in _menuItems)
             {
-                Console.WriteLine("Welcome to the video menu!\n" +
-                                  "Please select what you want to do:");
+                Console.WriteLine($"{menuItem.Key}. {menuItem.Value}");
             }
-            
         }
     }
 }
